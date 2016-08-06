005b3422acd06458588bd9631285ece149e8b6ab
Juan-manuel Clavijo <clavijo.j@gmail.com>
2016-08-05 20:17:56 -0700

    Updates to gilded rose file. New functions. refactored update-quality

--- clojure/spec/gilded_rose/core_spec.clj
+++ clojure/spec/gilded_rose/core_spec.clj
@@ -1,6 +1,24 @@
 (ns gilded-rose.core-spec
 (:require [speclj.core :refer :all]
-          [gilded-rose.core :refer [update-quality]]))
+          [gilded-rose.core :refer :all]))
 
 (describe "gilded rose"
-          (it "TODO - write tests later"))
+  (before-all (println "Start testing"))
+  
+  (after-all (println "Finished testing"))
+  
+  (it "regular-items are true specialty-items are false"
+      (should= (is-regular-item? {:sell-in 2 :quality 48 :name "Backstage passes 88"}) false)
+      (should= (is-regular-item? {:sell-in 2 :quality 48 :name "Sulfuras, Hand of Ragnaros"}) false)
+      (should= (is-regular-item? {:sell-in 2 :quality 48 :name "Aged Brie"}) false)
+      (should= (is-regular-item? {:sell-in 2 :quality 48 :name "Elixir of the Mongoose"}) true))
+   
+  (it "change-quality-of-item-by number specified but between 0-50"
+      (should= (:quality (change-quality-of-item-by {:name "Elixir", :sell-in 0, :quality 49} -2))
+               47)
+      (should= (:quality (change-quality-of-item-by {:name "Brie", :sell-in 0, :quality 49} 2))
+               50)      
+      (should= (:quality (change-quality-of-item-by {:name "Elixir", :sell-in 0, :quality 0} -1))
+               0))
+  )
+(run-specs)
--- clojure/src/gilded_rose/core.clj
+++ clojure/src/gilded_rose/core.clj
@@ -1,44 +1,65 @@
 (ns gilded-rose.core)
 
-(defn update-quality [items]
+(defn is-regular-item? [item]  ;;is this a regular/non-specialty item?
+  (if (or (= "Aged Brie" (:name item))
+          (some? (re-matches #"(?i)Sulfuras, Hand of Ragnaros" (:name item)))
+          (some? (re-matches #"(?i)Backstage passes.*" (:name item)))
+          (some? (re-matches #".*Conjured.*" (:name item)))) 
+    false
+    true))
+
+(defn backstage-pass? [item]  ;;there could be many different Backstage pass concerts. More regex rules would improve it
+  (if (some? (re-matches #"(?i)Backstage passes.*" (:name item)))
+    true
+    false))
+
+(defn backstage [item] ;;backstage passes have a lot of rules so it's better to isolate it
+  (if (backstage-pass? item)
+    (cond 
+      (< (:sell-in item) 0) (change-quality-of-item-by item (- (:quality item)))
+      (<= 0 (:sell-in item) 5) (change-quality-of-item-by item 3)
+      (<= 6 (:sell-in item) 10) (change-quality-of-item-by item 2)
+      (> (:sell-in item) 10) (change-quality-of-item-by item 1)
+      :else item) 
+    (throw (Exception. "Not a backstage item"))))
+
+(defn lower-sell-in [item] ;added this to make code more readable
+  (if (nil? (re-matches #"(?i)Sulfuras, Hand of Ragnaros" (:name item)));(not= "Sulfuras, Hand of Ragnaros" (:name item))
+    (merge item {:sell-in (dec (:sell-in item))})
+    item))
+
+(defn change-quality-of-item-by [item quality-delta] ;;this assures quality of items is within 0-50 range 
+  (if (some? (re-matches #"(?i)Sulfuras, Hand of Ragnaros" (:name item)))
+    (throw (Exception. "Trying to change quality of 'Sulfuras, Hand of Ragnaros'"))    
+    (cond
+      (<= (+ (:quality item) quality-delta) 0) (merge item {:quality 0})
+      (>= (+ (:quality item) quality-delta) 50) (merge item {:quality 50})
+      :else (merge item {:quality (+ (:quality item) quality-delta)}))))
+
+
+(defn update-quality [items] ;;updated function because that was nuts and *nearly impossible to read
   (map
-    (fn[item] (cond
-      (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
-        (merge item {:quality 0})
-      (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
-        (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
-          (merge item {:quality (inc (inc (:quality item)))})
-          (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
-            (merge item {:quality (inc (inc (inc (:quality item))))})
-            (if (< (:quality item) 50)
-              (merge item {:quality (inc (:quality item))})
-              item)))
-      (< (:sell-in item) 0)
-        (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
-          (merge item {:quality 0})
-          (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-            (merge item {:quality (- (:quality item) 2)})
-            item))
-      (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-        (merge item {:quality (dec (:quality item))})
-      :else item))
-  (map (fn [item]
-      (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
-        (merge item {:sell-in (dec (:sell-in item))})
-        item))
-  items)))
+    (fn[item] 
+      (cond
+        (is-regular-item? item) (change-quality-of-item-by item -1);if conjured item is *anything* update (change-quality-of-item-by item -2)
+        (= "Aged Brie" (:name item)) (change-quality-of-item-by item 1)
+        (backstage-pass? item) (backstage item)
+        (some? (re-matches #".*Conjured.*" (:name item))) (change-quality-of-item-by item -2);Conjured item?
+        :else item))
+      (map lower-sell-in
+          items)))
 
-(defn item [item-name, sell-in, quality]
-  {:name item-name, :sell-in sell-in, :quality quality})
+   
+;;didn't change the goblin's code ;-P
+    (defn item [item-name, sell-in, quality]
+      {:name item-name, :sell-in sell-in, :quality quality})
 
-(defn update-current-inventory[]
-  (let [inventory 
-    [
-      (item "+5 Dexterity Vest" 10 20)
-      (item "Aged Brie" 2 0)
-      (item "Elixir of the Mongoose" 5 7)
-      (item "Sulfuras, Hand Of Ragnaros" 0 80)
-      (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
-    ]]
-    (update-quality inventory)
-    ))
+    (defn update-current-inventory[]
+      (let [inventory [ (item "+5 Dexterity Vest" 10 20)
+                        (item "Aged Brie" 2 0)
+                        (item "Elixir of the Mongoose" 5 7)
+                        (item "Sulfuras, Hand Of Ragnaros" 0 80)
+                        (item "Backstage passes to a TAFKAL80ETC concert" 15 20)]]
+        (println inventory)
+    
+        (println (update-quality inventory))))
